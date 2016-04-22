9f1278023264989358c1bf52fbd8c19160d82cf2
musepuckel <fabian.schrader@web.de>
2016-04-22 23:40:51 +0200

    Player Script erstellt

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,15 +1,61 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public float Speed = 4f;
+    private float move = 0f;
+    public float tilt;
+
+    public int Health;
+    public int Money;
+    public Rigidbody PlayerRigidbody;
+
+    public float fireRate;
+    private float nextFire;
+
+    public GameObject prefab;
+    public float distance = 10.0f;
+
+    void Start()
+    {
+        PlayerRigidbody = this.GetComponent<Rigidbody>();
+    }
+
+
+    void FixedUpdate()
+    {
+        Move();
+        Shoot();
+    }
+
+    void Move()
+    {
+        var move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        PlayerRigidbody.position += move * Speed * Time.deltaTime;
+        //Debug.Log("Horizontal: " + Input.GetAxis("Horizontal").ToString());
+        //Debug.Log("Vertical: " + Input.GetAxis("Vertical").ToString());
+
+
+    }
+
+    void Shoot()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            Debug.Log("Shoot-Funktion");
+            nextFire = Time.time + fireRate;
+
+
+
+            var position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+            position = Camera.main.ScreenToWorldPoint(position);
+            var go = Instantiate(prefab, transform.position, Quaternion.identity) as GameObject;
+            go.transform.LookAt(position);
+            Debug.Log(position);
+            go.GetComponent<Rigidbody>().AddForce(go.transform.forward * 1000);
+
+        }
+    }
 }
