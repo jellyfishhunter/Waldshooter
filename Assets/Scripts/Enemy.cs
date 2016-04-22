ec0172dd4ae645fb040f1586581239e6fbb1594e
Alexander Gebenroth <jellyfishhunter@gmail.com>
2016-04-22 21:53:41 +0200

    Enemy-Skript
    in prototype 1 läuft ein tic tac zu einem "Base Tree"-Würfel

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,13 +3,38 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour {
 
+    public int hp;
+    public GameObject dropObject;
+
+    Transform targetTransform;
+    NavMeshAgent agent;
+
 	// Use this for initialization
 	void Start () {
-	
+        agent = GetComponent<NavMeshAgent>();
+        targetTransform = chooseTarget();
+        agent.destination = targetTransform.position;
+        agent.Resume();
 	}
+
+    public void initialize()
+    {
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    // TODO add player and building detection
+    Transform chooseTarget()
+    {
+        return GameObject.FindGameObjectWithTag("Base Tree").transform;
+    }
+
+    void shoot ()
+    {
+
+    }
 }
