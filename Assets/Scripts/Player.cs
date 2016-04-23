1122d196bb7f58cfeef2452d4b47d1f05879d069
maltooo <ma.boedeker@gmail.com>
2016-04-23 12:04:28 +0200

    Player script modifiziert, Szene erstellt, playerasset erstellt

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -23,11 +23,42 @@ public class Player : MonoBehaviour
         PlayerRigidbody = this.GetComponent<Rigidbody>();
     }
 
+	void LookAtMouse () 
+	{
+		// Generate a plane that intersects the transform's position with an upwards normal.
+		Plane playerPlane = new Plane(Vector3.up, transform.position);
+
+		// Generate a ray from the cursor position
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+		// Determine the point where the cursor ray intersects the plane.
+		// This will be the point that the object must look towards to be looking at the mouse.
+		// Raycasting to a Plane object only gives us a distance, so we'll have to take the distance,
+		//   then find the point along that ray that meets that distance.  This will be the point
+		//   to look at.
+		float hitdist = 0.0f;
+		// If the ray is parallel to the plane, Raycast will return false.
+		if (playerPlane.Raycast (ray, out hitdist)) 
+		{
+			// Get the point along the ray that hits the calculated distance.
+			Vector3 targetPoint = ray.GetPoint(hitdist);
+
+			// Determine the target rotation.  This is the rotation if the transform looks at the target point.
+			Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+
+			// Smoothly rotate towards the target point.
+			transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 20* Time.deltaTime);
+		}
+	}
+
+
 
     void FixedUpdate()
     {
         Move();
         Shoot();
+		//LookAtMouse (); 
+		 
     }
 
     void Move()
@@ -37,6 +68,7 @@ public class Player : MonoBehaviour
         //Debug.Log("Horizontal: " + Input.GetAxis("Horizontal").ToString());
         //Debug.Log("Vertical: " + Input.GetAxis("Vertical").ToString());
 
+		transform.rotation = Quaternion.LookRotation(move);
 
     }
 
@@ -51,8 +83,9 @@ public class Player : MonoBehaviour
 
             var position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
             position = Camera.main.ScreenToWorldPoint(position);
-            var go = Instantiate(prefab, transform.position, Quaternion.identity) as GameObject;
-            go.transform.LookAt(position);
+            
+			var go = Instantiate(prefab, transform.position, transform.rotation) as GameObject;
+           // go.transform.LookAt(position);
             Debug.Log(position);
             go.GetComponent<Rigidbody>().AddForce(go.transform.forward * 1000);
 
